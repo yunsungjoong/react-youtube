d0912a010a9b3f7d70fc21581522305e755a2ec5
yunsungjoong <yun_sj94@naver.com>
2023-08-13 12:47:41 +0900

    유뷰트 클론코딩 push

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { formatAgo } from '../utill/date';
+import { useNavigate } from 'react-router-dom';
+
+export default function VideoCard({ video , type }) {
+    const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+    const navigate= useNavigate();
+    const isList = type === 'list'
+    return (
+     <li 
+        className={isList ? 'flex gap-1 m-2' : ''}
+        onClick={() => {
+                navigate(`/videos/watch/${video.id}`, { state: { video } });
+            }}
+        > 
+        <img className={isList ? 'w-60 mr-2' : 'w-full'} src={thumbnails.medium.url} alt={title}/>
+        <div>
+            <p className='font-semibold my-2 line-clamp-2 rounded-md'>{title}</p>
+            <p className='text-sm opacity-80'>{channelTitle}</p>
+            <p className='text-sm opacity-80'>{formatAgo(publishedAt)}</p>
+        </div>
+     </li>
+    );  
+}
+
